18d939eeab10deb2b277abb3be2d914272a7a443
KaterinaBondarenko <kat9bondarenko@gmail.com>
2022-02-14 17:17:56 +0200

    summary

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Section from './components/section/Section';
+import FeedbackOptions from './components/feedbackOptions/FeedbackOptions';
+import Statistics from './components/statistics/Statistics';
+
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+
+  const handleFeedback = e => {
+    const type = e.target.name;
+    switch (type) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
+  };
+  const countTotalFeedback = () => {
+    return bad + neutral + good;
+  };
+  const countPositiveFeedbackPercentage = () => {
+    return ((good / countTotalFeedback()) * 100).toFixed(0);
+  };
+
+  const options = ['bad', 'good', 'neutral'];
+  return (
+    <Section titel="Please leave feedback">
+      <FeedbackOptions options={options} onLeaveFeedback={handleFeedback} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={countTotalFeedback()}
+        positivePercentage={countPositiveFeedbackPercentage()}
+      />
+    </Section>
+  );
+}
